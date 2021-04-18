b1a989fe36c9c76bbab72d26f767b48e48efe526
20460546Wan <20460546@life.hkbu.edu.hk>
2021-04-18 14:41:57 +0800

    created weight management function

--- chatbot.py
+++ chatbot.py
@@ -1,14 +1,27 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
+
 import configparser
 import logging
+import csv
 import telebot
-
+import matplotlib
+import tempfile
+import numpy as np
+import pandas as pd
+import pendulum
+from pandas.plotting import register_matplotlib_converters
 from pymongo import MongoClient
 
+import matplotlib.pyplot as plt
+
+############################################### Main ##################################################
 
 def main():
     
+    matplotlib.use("agg")
+    register_matplotlib_converters()
+
     # Load your token and create an Updater for your Bot
     config = configparser.ConfigParser()
     config.read('config.ini')
@@ -19,24 +32,33 @@ def main():
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
 
+    dispatcher.add_handler(CommandHandler("add", add))
+
 ############################## Handlers for workout guide function #######################################
     updater.dispatcher.add_handler(CommandHandler('workout', workout_command))
     updater.dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
     updater.dispatcher.add_handler(CallbackQueryHandler(advanced, pattern='advanced'))
     updater.dispatcher.add_handler(CallbackQueryHandler(intermediate, pattern='intermediate'))
     updater.dispatcher.add_handler(CallbackQueryHandler(beginner, pattern='beginner'))
-    dispatcher.add_handler(CommandHandler("add", add))
-##########################################################################################################      
+##########################################################################################################   
+
+############################## Handlers for weight management function ###################################
+    dispatcher.add_handler(CommandHandler("start", weight_start))
+    dispatcher.add_handler(CommandHandler("weight", weight_store))
+    dispatcher.add_handler(CommandHandler("height", height_store))
+##########################################################################################################   
+   
     updater.dispatcher.add_error_handler(error)
     
     updater.start_polling()
     updater.idle()
 
+############################################### Main ##################################################
+
+
 def add(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /add is issued."""
-    
     client = MongoClient("mongodb+srv://comp7940group1:comp7940group1@cluster0.vyq4q.mongodb.net/Message-DB?retryWrites=true&w=majority")
-
     try: 
         db = client.add
         logging.info(context.args[0])
@@ -45,6 +67,45 @@ def add(update: Update, context: CallbackContext) -> None:
         update.message.reply_text("added")
     except (IndexError, ValueError):
         update.message.reply_text('Usage: /add <keyword>')
+
+
+############################## weight management function ################################################
+
+def weight_start(update: Update, context: CallbackContext):
+    """Send a welcome message."""
+    update.message.reply_text(
+        "Hi! Just type in your current height (/height) in cm and weight (/weight) in Kg and I'll store it for you!"
+    )
+
+def weight_store(update: Update, context: CallbackContext):
+    """Send a number (weight) when the command /weight is issued."""
+    client = MongoClient("mongodb+srv://comp7940group1:comp7940group1@cluster0.vyq4q.mongodb.net/Message-DB?retryWrites=true&w=majority")
+
+    try: 
+        db = client.user
+        logging.info(context.args[0])
+        number = context.args[0]   # /add keyword <-- this should store the keyword
+        result = db.weight.insert_one({'weight': number})
+        update.message.reply_text("Weight is stored successfully")
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /weight <keyword>')
+    
+def height_store(update: Update, context: CallbackContext):
+    """Send a number (weight) when the command /weight is issued."""
+    client = MongoClient("mongodb+srv://comp7940group1:comp7940group1@cluster0.vyq4q.mongodb.net/Message-DB?retryWrites=true&w=majority")
+
+    try: 
+        db = client.user
+        logging.info(context.args[0])
+        number = context.args[0]   # /add keyword <-- this should store the keyword
+        result = db.height.insert_one({'height': number})
+        update.message.reply_text("Height is stored successfully")
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /height <keyword>')
+
+############################## weight management function ################################################
+
+
 ##############################Workout Guide Function #####################################################
 def workout_command(bot, update):
     bot.message.reply_text(main_menu_message(),
